cd73492535ce30e8ebc02be1c37f6c90a2beae45
Nicole Sattler <nicole.sattler@live.com>
2017-06-09 00:03:58 +0000

    Added speech recognition to searchbar

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 /* global $ */
+/* global webkitSpeechRecognition */
 
 $(document).ready(function(){
     $('#searchTerm').on('click', function(){
@@ -16,4 +17,31 @@ $(document).ready(function(){
     $('#lucky').on('click', function(){
         window.open('https://www.google.com/doodles', '_self');
     });
+    
+    $('#mic').on('click', startDictation);
+    
+    function startDictation() {
+
+    if (window.hasOwnProperty('webkitSpeechRecognition')) {
+
+      var recognition = new webkitSpeechRecognition();
+
+      recognition.continuous = false;
+      recognition.interimResults = false;
+
+      recognition.lang = "en-US";
+      recognition.start();
+
+      recognition.onresult = function(e) {
+        document.getElementById('searchbar').value
+                                 = e.results[0][0].transcript;
+        recognition.stop();
+      };
+
+      recognition.onerror = function(e) {
+        recognition.stop();
+      }
+
+    }
+  }
 });
